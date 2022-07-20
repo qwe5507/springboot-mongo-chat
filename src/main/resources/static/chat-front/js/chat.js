b99bbfc6bb54563473e6a6ba39b1f0ada78418b0
qwe5507 <apvmffkdls@gmail.com>
2022-07-20 22:33:01 +0900

    feat : 기존 채팅 내역 출력

--- src/main/resources/static/chat-front/js/chat.js
+++ src/main/resources/static/chat-front/js/chat.js
@@ -3,39 +3,51 @@ eventSource.onmessage = (event) => {
     console.log(1, event);
     const data = JSON.parse(event.data);
     console.log(2, data);
+    initMessage(data);
 }
 
 
-function getSendMsgBox(msg) {
+function getSendMsgBox(msg, time) {
     return `<div class="sent_msg">
                 <p>${msg}</p>
-                <span class="time_date"> 11:18 | Today</span>
+                <span class="time_date"> ${time}</span>
             </div>`;
 }
 
-document.querySelector("#chat-outgoing-button").addEventListener("click", () => {
+function initMessage(data) {
     let chatBox = document.querySelector("#chat-box");
     let msgInput = document.querySelector("#chat-outgoing-msg");
 
     let chatOutgoingBox = document.createElement("div");
     chatOutgoingBox.className = "outgoing_msg";
 
-    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value);
+    chatOutgoingBox.innerHTML = getSendMsgBox(data.msg, data.createAt);
     chatBox.append(chatOutgoingBox);
     msgInput.value = "";
+}
+
+function addMessage() {
+    let chatBox = document.querySelector("#chat-box");
+    let msgInput = document.querySelector("#chat-outgoing-msg");
+
+    let chatOutgoingBox = document.createElement("div");
+    chatOutgoingBox.className = "outgoing_msg";
+
+    let date = new Date();
+    let now = date.getHours() + ":" + date.getMinutes() + " | " + date.getMonth()+"/" + date.getDate();
+
+    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value, now);
+    chatBox.append(chatOutgoingBox);
+    msgInput.value = "";
+}
+
+document.querySelector("#chat-outgoing-button").addEventListener("click", () => {
+    addMessage();
 });
 
 
 document.querySelector("#chat-outgoing-msg").addEventListener("keydown", (e) => {
     if (e.keyCode === 13) {
-        let chatBox = document.querySelector("#chat-box");
-        let msgInput = document.querySelector("#chat-outgoing-msg");
-
-        let chatOutgoingBox = document.createElement("div");
-        chatOutgoingBox.className = "outgoing_msg";
-
-        chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value);
-        chatBox.append(chatOutgoingBox);
-        msgInput.value = "";
+        addMessage();
     }
 });
